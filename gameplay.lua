6649dc29fc43d9294e5ba4e8686970e4982e1c47
ShaneKavinda <shanekavinda1999@gmail.com>
2023-10-04 15:50:27 +0530

    added a main menu button and fixed pause button

--- gameplay.lua
+++ gameplay.lua
@@ -15,6 +15,8 @@ local cellSize = math.min(display.actualContentHeight, display.actualContentWidt
 local cellGroup = display.newGroup()  -- Group to hold cell objects
 local isPaused = false  -- Flag to pause or resume the simulation
 
+local sceneGroup  -- Declare sceneGroup to make it accessible across functions
+
 -- Function to initialize the grid with random values
 local function initializeGridRandomly()
     for i = 1, gridSize do
@@ -109,21 +111,79 @@ local function updateDisplay()
     end
 end
 
+-- Store the reference to the active timer
+local activeTimer = nil
+
 -- Function to advance the simulation
 local function advanceSimulation()
-    calculateNextState()
-    updateDisplay()
+   
+
+    -- Clear the active timer if it exists
+    if activeTimer then
+        timer.cancel(activeTimer)
+    end
 
     if not isPaused then
         local delay = animationSpeed * 1000  -- Convert animation speed to milliseconds
-        timer.performWithDelay(delay, advanceSimulation)
+        calculateNextState()
+        updateDisplay()
+        -- Store the reference to the new timer
+        activeTimer = timer.performWithDelay(delay, advanceSimulation)
     end
 end
 
--- Function to toggle pause/resume of the simulation
-local function togglePause()
+
+-- Function to handle the "Pause" button tap event
+local function pauseButtonTap(event)
     isPaused = not isPaused
+    local sceneGroup = event.target.parent  -- Get the parent group of the button
+
+    local pauseButton = sceneGroup.pauseButton
+    local saveStateButton = sceneGroup.saveStateButton
+
+    if isPaused then
+        pauseButton:setLabel("Resume")
+        saveStateButton:setEnabled(true)  -- Enable the "Save State" button when paused
+    else
+        pauseButton:setLabel("Pause")
+        saveStateButton:setEnabled(false)  -- Disable the "Save State" button when resumed
+        advanceSimulation()  -- Resume the simulation when unpaused
+    end
+
+    return true  -- Prevents touch propagation to objects below the button
 end
+
+-- Function to handle the "Save State" button tap event
+local function saveStateButtonTap(event)
+    local sceneGroup = event.target.parent  -- Get the parent group of the button
+    local filePath = system.pathForFile("game_state.txt", system.DocumentsDirectory)
+    local file = io.open(filePath, "w")
+
+    if file then
+        for i = 1, gridSize do
+            for j = 1, gridSize do
+                file:write(grid[i][j] .. " ")
+            end
+            file:write("\n")
+        end
+        io.close(file)
+        print("Game state saved to game_state.txt")
+    else
+        print("Error: Unable to open the file for writing")
+    end
+
+    return true  -- Prevents touch propagation to objects below the button
+end
+
+-- Function to handle the "Main Menu" button tap event
+local function gotoMainMenu(event)
+     -- store the grid size and the animation speed to be used throughout the app
+     composer.setVariable("gridSize", gridSize)
+     composer.setVariable("animationSpeed", animationSpeed)
+    -- go to the scene "Menu"
+    composer.gotoScene( "menu",{effect="fade", time=500} )
+end
+
 
 function scene:create(event)
     local sceneGroup = self.view
@@ -134,11 +194,32 @@ function scene:create(event)
 
     local pauseButton = widget.newButton({
         label = "Pause",
-        x = display.contentCenterX,
+        x = display.contentCenterX/4,
         y = display.contentHeight - 50,
-        onPress = togglePause,
+        onPress = pauseButtonTap,
     })
     sceneGroup:insert(pauseButton)
+
+    local saveStateButton = widget.newButton({  -- Create a "Save State" button
+        label = "Save State",
+        x = display.contentCenterX + display.actualContentWidth/4,
+        y = display.contentHeight - 50,
+        enabled = false,  -- Initially disabled when not paused
+        onPress = saveStateButtonTap,
+    })
+    sceneGroup:insert(saveStateButton)
+
+    local homeButton = widget.newButton( {
+        label = "Main Menu",
+        x = display.contentCenterX/4,
+        y = display.contentHeight - 20,
+        onPress = gotoMainMenu,
+    } )
+    sceneGroup:insert(homeButton)
+
+    -- Store the buttons in scene properties
+    sceneGroup.pauseButton = pauseButton
+    sceneGroup.saveStateButton = saveStateButton
 
     advanceSimulation()
 end
